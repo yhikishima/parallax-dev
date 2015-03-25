f0e1ba1a8512192955dcc646175ec50bc8f2ee98
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-03-25 09:14:33 +0900

    add parallax plugin

---
+++ 01.parallaxJs/js/app.js
@@ -0,0 +1,18 @@
+(function() {
+  $('.parallax-window-01').parallax({
+    imageSrc: 'img/sample_img01.jpg'
+  });
+
+  $('.parallax-window-02').parallax({
+    imageSrc: 'img/sample_img02.jpg'
+  });
+
+  $('.parallax-window-03').parallax({
+    imageSrc: 'img/sample_img03.jpg'
+  });
+
+  $('.parallax-window-04').parallax({
+    imageSrc: 'img/sample_img04.jpg'
+  });
+
+})();
